a42ed63525e3563922ceedfd6d127c63eb70393d
Tuan Pham <tuan.pham@barrelny.com>
2022-10-28 18:11:31 +0000

    Add vim setup script

---
+++ scripts/vim_setup.sh
@@ -0,0 +1,5 @@
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+mkdir -p ~/.config/nvim/
+cp ~/.dotfiles/config/init.vim ~/.config/nvim
