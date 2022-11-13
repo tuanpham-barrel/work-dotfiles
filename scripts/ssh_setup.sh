83aa3756373506d9e9244b8fcba6b5f1e9b5b302
tuanpham-barrel <107269100+tuanpham-barrel@users.noreply.github.com>
2022-11-13 23:21:41 +0700

    Add known_hosts support

--- scripts/ssh_setup.sh
+++ scripts/ssh_setup.sh
@@ -9,3 +9,8 @@ if [ -n "$SSH_PUBLIC" ]; then
   echo "$SSH_PUBLIC" > ~/.ssh/id_rsa.pub
   chmod 644 ~/.ssh/id_rsa.pub
 fi
+
+if [ -n "$SSH_KNOWN_HOSTS" ]; then
+  echo "$SSH_KNOWN_HOSTS" | base64 -d > ~/.ssh/known_hosts
+  chmod 644 ~/.ssh/known_hosts
+fi
